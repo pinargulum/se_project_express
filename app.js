31a394dc3502bc0dad69f2785159acc25c4fb262
Pinar Gulum <pinargulum@hotmail.com>
2025-01-03 18:02:14 -0500

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const { default: mongoose } = require('mongoose')
-const app = express()
-const router = express.Router()
-
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const app = express();
+;const router = express.Router()
 const { PORT = 3001 } = process.env
+
 
 
 // i will create user routs on project root
@@ -14,12 +14,11 @@ app.post('/api/users/', (reg, res) => {
   res.status(200).json({ message: 'create user' })
 })
 //
-mongoose
-.connect('mongodb://127.0.0.1:27017/wtwr_db')
+
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
 .then(() => {
   console.log('db connected');
 }).catch(console.error);
-
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`)
 })
