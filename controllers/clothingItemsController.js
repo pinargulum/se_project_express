cc1b7e12f2fdc84d6a3244891aa090849176963b
Pinar Gulum <pinargulum@hotmail.com>
2025-01-08 01:23:28 -0500

    update

--- controllers/clothingItemsController.js
+++ controllers/clothingItemsController.js
@@ -1,7 +1,6 @@
 const ClothingItem = require('../models/clothingItems')
-//const ClothingItem = require('../models/clothingItems')
 
-// GET ALL THE USERS
+// get all the items
 const getItems = (req, res) => {
   ClothingItem.find({})
     .then(clothingItems => {
@@ -12,29 +11,39 @@ const getItems = (req, res) => {
       return res.status(400).send({ message: err.message })
     })
 }
-
-// GET SINGLE USER
-const deleteItem = (req, res) => {
-  const { itemId } = req.params
-  ClothingItem.findByIdAndDelete(itemId)
-  .orFail()
-  .then((item) => {
-    if (item == null)
-    res.send(item);
-  })
-    .catch((err) => {
+// create a new item
+const createItem = (req, res) => {
+  const { name, imageUrl, weather } = req.body
+  ClothingItem.create({ name, imageUrl, weather })
+    .then(item => {
+      res.status(201).send({ data: item })
+    })
+    .catch(err => {
       console.error(err)
       return res.status(400).send({ message: err.message })
     })
 }
-
-// POST NEW USER
-const createItem = (req, res) => {
-  const { name, imageUrl, weather } = req.body
-  ClothingItem.create({ name, imageUrl, weather })
-  //console.log(req.user._id)
+//delete item by id
+const deleteItem = (req, res) => {
+  const { itemId } = req.params
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
     .then(item => {
-      res.status(201).send({data: item})
+      if (item == null) res.send(item)
+    })
+    .catch(err => {
+      console.error(err)
+      return res.status(400).send({ message: err.message })
+    })
+}
+// update items by id
+const updateItem = (req, res) => {
+  const { itemId } = req.params
+  const { name, weather, imageUrl } = req.body
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { name, weather, imageUrl } })
+    .orFail()
+    .then(item => {
+      res.status(200).send({ data: item })
     })
     .catch(err => {
       console.error(err)
@@ -42,4 +51,4 @@ const createItem = (req, res) => {
     })
 }
 
-module.exports = { getItems, createItem, deleteItem }
+module.exports = { getItems, createItem, deleteItem, updateItem }
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -3,16 +3,18 @@ const router = express.Router()
 const {
   getItems,
   createItem,
-  deleteItem
+  deleteItem,
+  updateItem
 } = require('../controllers/clothingItemsController')
 
-//GET ALL THE ITEMS
+
 router.get('/', getItems)
 
-// POST ITEMS
+
 router.post('/', createItem)
 
-//DELETE ITEMS
+
 router.delete('/:itemId', deleteItem)
+router.put('/:itemId', updateItem)
 
 module.exports = router
