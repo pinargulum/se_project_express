2d42cfe9b9bbc1fd01a8d0eb808574762e96b3e9
Pinar Gulum <pinargulum@hotmail.com>
2025-01-10 15:18:46 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ const { NOT_FOUND } = require("../utils/constants")
 router.use('/users', userRauter)
 router.use('/items', itemRauter)
 router.use((req, res) => {
-  return res.status(NOT_FOUND).send({ message: 'Page not found' })
+  res.status(NOT_FOUND).send({ message: 'Page not found' })
 })
 
 module.exports = router
