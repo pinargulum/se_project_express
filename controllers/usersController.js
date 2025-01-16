e9398b569c0d0fdd69c1d81c0c1b713f9b9e2267
Pinar Gulum <pinargulum@hotmail.com>
2025-01-16 13:57:42 -0500

    update

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,36 +10,9 @@ const {
   NOT_FOUND,
 } = require("../utils/constants");
 
-const login = (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res
-      .status(VALIDATION_ERROR)
-      .send({ message: "Email and password are required" })
-  }
-  User.findUserByCredentials(email, password)
-  .then((user) => {
-    User.findOne({ email });
-    if (!user) {
-      return res.status(NOT_FOUND).send({ message: "User not found" });
-    }
-    const isMatch = bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res
-        .status(VALIDATION_ERROR)
-        .send({ message: "Invalid password" });
-    }
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-        expiresIn: "7d",
-      });
-      res.status(200).send({ message: "Login Succesful", token })
-    })
 
-    .catch((err) => {
-      res.status(SERVER_ERROR).send({ message: "Please login to continue" });
-    });
-};
-// GET ALL THE USERS
+
+// get all the users
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => {
@@ -53,7 +26,7 @@ const getUsers = (req, res) => {
     });
 };
 
-// GET SINGLE USER
+// get single user
 const getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
@@ -83,7 +56,7 @@ const getUser = (req, res) => {
     });
 };
 
-// POST NEW USER
+// create a user
 const createUser = (req, res) => {
   const { name, avatar, email, password } = req.body;
   User.findOne({ email })
@@ -95,7 +68,10 @@ const createUser = (req, res) => {
       }
       bcrypt
         .hash(req.body.password, 10)
-        .then((hash) => User.create({ name, avatar, email, password: hash }))
+        .then((hash) => User.create({
+          email: req.body.email,
+          password: hash
+        }))
         .then((user) => {
           res.status(201).send(user);
         });
@@ -113,5 +89,20 @@ const createUser = (req, res) => {
         .send({ message: "An error has occurred on the server." });
     });
 };
-
-module.exports = { login, getUsers, getUser, createUser };
+/*
+const login = (req, res) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      res.send({
+        token: jwt.sign({ _id: user._id }, "super-strong-secret", {
+          expiresIn: "7d",
+        }),
+      });
+    })
+    .catch((err) => {
+      res.status(SERVER_ERROR).send({ message: "Please login to continue" });
+    });
+};
+*/
+module.exports = { getUsers, getUser, createUser };
--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ const express = require('express')
 const router = express.Router()
 
 const {
-  login,
+  //login,
   getUsers,
   getUser,
   createUser
 } = require('../controllers/usersController')
-router.post("/", login)
+//router.post("/", login)
 router.get("/", getUsers)
 router.post("/", createUser)
 router.get('/:userId', getUser)
