30a31b0a67b241a7b5666506f003265bdd9a4bde
Pinar Gulum <pinargulum@hotmail.com>
2025-01-19 00:47:10 -0500

    Update clothingItems.js

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -15,7 +15,7 @@ const {
 
 router.get('/', getItems)
 router.post('/', createItem)
-router.delete('/:itemId', deleteItem)
+router.delete('/:itemId', auth, deleteItem)
 router.put('/:itemId/likes', likeItem)
 router.delete('/:itemId/likes', dislikeItem)
 
