4ea603bc0958ef0396712c33bbb20bed5b86d0f3
Pinar Gulum <pinargulum@hotmail.com>
2025-03-28 20:42:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const cors = require("cors");
-const helmet = require('helmet')
-const { errors } = require('celebrate');
+const helmet = require("helmet");
+const { errors } = require("celebrate");
+require("dotenv").config();
 const mainRouter = require("./routes/index");
 
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-
-
-require('dotenv').config();
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -19,22 +17,23 @@ mongoose
   })
   .catch(console.error);
 
-
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
-app.get('/crash-test', () => {
+app.get("/crash-test", () => {
   setTimeout(() => {
-    throw new Error('Server will crash now');
+    throw new Error("Server will crash now");
   }, 0);
 });
 app.use("/", mainRouter);
 app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
-  console.error(err);
-res.status(err.statusCode).send({ message: err.message });
+  const statusCode = err.statusCode || 500;
+  const message = statusCode === 500 ? "An error has occurred on server" : err.message;
+  res.status(statusCode).send({ message });
+  next();
 });
 
 app.listen(PORT, () => {
